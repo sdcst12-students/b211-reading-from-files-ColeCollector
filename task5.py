7f85448f6edd9c55519660360fa18dbf66fcd356
ColeCollector <colen827@deltalearns.ca>
2023-10-24 13:02:10 -0700

    a

--- task5.py
+++ task5.py
@@ -1,32 +1,6 @@
-"""
-Read the data from the file task02.csv
-Allow the user to search for a stock symbol.
-If the stock symbol is found, display the name of the company
-If a multiple stocks match the symbol, say there are multiple stocks available
-If there is no match, say "no match found"
-
-Example:
-Enter stock symbol: AA
-There are 21 stocks with that symbol
-Enter stock symbol: AAPL
-Apple Inc.
-Enter stock symbol: YANG
-No matches
-"""
-
-
 while True:
-    name = (input("\nEnter a stock symbol: ")).upper()
-    lineData = open("task5.csv","r").read().split("\n")
-    newData = []
-    stocks = []
-    number = 0
-    for i in lineData:
-        newData.append(i.split(","))
-    for i in newData:
-        if name in i[0]:
-            number +=1
-            stocks.append(i)
-    if number == 0:print("No matches")
-    elif number == 1:print((stocks[0][1]).replace('"', ""))
-    else: print(f"There are {number} stocks with that symbol")
+    name = (input("\nEnter a stock symbol: ")).upper();lineData = open("task5.csv","r").read().split("\n");newData = [];stocks = []
+    for i in lineData:newData.append(i.split(","))
+    stocks = [i for i in newData if name in i[0]]
+    print("No matches") if len(stocks) == 0 else print(f"There are {len(stocks)} stocks with that symbol")
+    if len(stocks) == 1:print((stocks[0][1]).replace('"', ""))
